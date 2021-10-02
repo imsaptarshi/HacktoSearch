18a682f91ec66030edfdb2689304ef20efc852d6
Your Name <avneeshagarwal0612@gmail.com>
2021-10-02 14:34:27 +0530

    improved lighthouse scores

--- src/Components/Footer/footer.component.jsx
+++ src/Components/Footer/footer.component.jsx
@@ -20,39 +20,41 @@ function Footer() {
   );
 
   return (
-    <Flex
-      justify="center"
-      bg="whiteAlpha.300"
-      w="full"
-      py="3"
-      position="absolute"
-      bottom="0"
-    >
+    <footer>
       <Flex
-        alignItems="center"
-        w={{ base: "full", xl: "1500px" }}
-        px={{ base: "8", md: "12", lg: "20", xl: "40" }}
-        justify="space-between"
+        justify="center"
+        bg="whiteAlpha.300"
+        w="full"
+        py="3"
+        position="absolute"
+        bottom="0"
       >
-        <Text fontWeight="medium" color="white" fontSize="lg">
-          #happyhacking
-        </Text>
-        <Flex alignItems="center" experimental_spaceX="2">
-          <SocialLink
-            icon={<GitHubSocialIcon w={7} h={7} />}
-            link="https://github.com/imsaptarshi"
-          />
-          <SocialLink
-            icon={<TwitterSocialIcon w={7} h={7} />}
-            link="https://twitter.com/imsaptarshiii"
-          />
-          <SocialLink
-            icon={<DiscordSocialIcon w={7} h={7} />}
-            link="https://discord.gg/VFfG7UTn5J"
-          />
+        <Flex
+          alignItems="center"
+          w={{ base: "full", xl: "1500px" }}
+          px={{ base: "8", md: "12", lg: "20", xl: "40" }}
+          justify="space-between"
+        >
+          <Text fontWeight="medium" color="white" fontSize="lg">
+            #happyhacking
+          </Text>
+          <Flex alignItems="center" experimental_spaceX="2">
+            <SocialLink
+              icon={<GitHubSocialIcon w={7} h={7} />}
+              link="https://github.com/imsaptarshi"
+            />
+            <SocialLink
+              icon={<TwitterSocialIcon w={7} h={7} />}
+              link="https://twitter.com/imsaptarshiii"
+            />
+            <SocialLink
+              icon={<DiscordSocialIcon w={7} h={7} />}
+              link="https://discord.gg/VFfG7UTn5J"
+            />
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </footer>
   );
 }
 
